738ac58e0e1dbca1f92561cbe43aa07da5b89277
Kajol232 <ridamot123@yahoo.com>
2021-12-24 12:53:33 +0100

    first commit

---
+++ BusRoute.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Challenge1
+{
+    public class BusRoute
+    {
+        public int FindRoutes(int[][] routes, int source, int target)
+        {
+            int min = 0;
+            ArrayList list = FindIndex(routes, source);
+            foreach(int s in list)
+            {
+                Node n = DrawTree(routes, new Node(0), source);
+                int path = Transverse(n, target);
+                if(path < min)
+                {
+                    min = path;
+                }
+            }
+
+            return min;
+
+        }
+
+        public int Transverse(Node n, int target)
+        {
+
+
+        }
+        public Node DrawTree(int[][] routes,Node n, int source)
+        {
+            ArrayList list = FindIndex(routes,source);
+
+            foreach(int s in list)
+            {
+                int index = Array.IndexOf(routes[s], source);
+                n.key = (routes[s][index]);
+                for(int i = 0; i < routes[s].Length; i++)
+                {
+                    if(routes[s][i] != n.key)
+                    {
+                        if (n.left == null)
+                        {
+
+                            n.left = new Node(routes[s][i]);
+                            DrawTree(routes, n.left, n.left.key);
+                        }
+                        else
+                        {
+                            n.right = new Node(routes[s][i]);
+                            DrawTree(routes, n.right, n.right.key);
+                        }
+
+                    }
+                    
+                }
+
+            }
+
+            return n;
+        }
+        private ArrayList FindIndex(int[][] routes, int source)
+        {
+            ArrayList sourceIndexList = new ArrayList();
+            //ArrayList targetIndexList = new ArrayList();
+            for (int i = 0; i < routes.Length; i++)
+            {
+                for (int j = 0; j < routes[i].Length; j++)
+                {
+                    if (routes[i][j] == source)
+                    {
+                        sourceIndexList.Add(i);
+                        break;
+                    }
+                }
+            }
+            return sourceIndexList;
+        }
+    }
+
+    public class Node
+    {
+        public int key { get; set; } 
+        public Node left { get; set; }
+        public Node right { get; set; }
+
+        public Node(int key)
+        {
+            this.key = key;
+            left = null;
+            right = null;
+        }
+    }
+}
---
+++ ValidSubstring.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Challenge1
+{
+    public class ValidSubstring
+    {
+        public static int CheckLongest(string s)
+        {
+            int max = 0;
+            for(int i = 0; i < s.Length; i++)
+            {
+                for (int j = 2; j + i < s.Length; j++)
+                {
+                    String newString = s.Substring(i, j);
+                    if (IsValid(newString))
+                    {
+                        if (newString.Length > max)
+                        {
+                            max = newString.Length;
+                        }
+                    }
+
+                }
+                
+            }
+
+            return max;
+        }
+
+        private static bool IsValid(string code)
+        {
+            bool isValid = false;
+            Stack<char> charStack = new Stack<char>();
+            foreach (var c in code)
+            {
+                if (c.Equals('('))
+                {
+                    charStack.Push(c);
+                }
+
+                if (c.Equals(')'))
+                {
+                    if (charStack.Count > 0)
+                    {
+                        charStack.Pop();
+                    }
+                    else
+                    {
+                        return isValid;
+                    }
+
+                }
+
+            }
+
+            if (charStack.Count == 0)
+            {
+                isValid = true;
+            }
+
+            return isValid;
+
+        }
+    }
+}
+
